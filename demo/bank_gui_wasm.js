0eb0508648add4fff403537d84b5db0a077f3ba3
Yaraslaut <mail@yaraslau.com>
2026-07-03 16:07:30 +0000

    Deploying to gh-pages from @ LASTRADA-Software/morph@bed488a9131cd3b1a12bd0604413bdd69f4bcdf2 🚀

--- demo/bank_gui_wasm.js
+++ demo/bank_gui_wasm.js
@@ -13289,9 +13289,9 @@ var ___cxa_can_catch = createExportWrapper("__cxa_can_catch");
 
 var ___cxa_is_pointer_type = createExportWrapper("__cxa_is_pointer_type");
 
-var ___start_em_js = Module["___start_em_js"] = 10163e3;
+var ___start_em_js = Module["___start_em_js"] = 10163016;
 
-var ___stop_em_js = Module["___stop_em_js"] = 10164090;
+var ___stop_em_js = Module["___stop_em_js"] = 10164106;
 
 function invoke_viiii(index, a1, a2, a3, a4) {
  var sp = stackSave();
