ee5d6524d5879606c36634714c5e5f431be706e1
Yaraslaut <mail@yaraslau.com>
2026-09-19 08:23:38 +0000

    Deploying to gh-pages from @ LASTRADA-Software/morph@3e8f59f27ea769210d75b47d53b6638924be4e76 🚀

--- d8/d3a/locale__format_8hpp.js
+++ d8/d3a/locale__format_8hpp.js
@@ -1,5 +1,6 @@
 var locale__format_8hpp =
 [
+    [ "morph::render::detail::groupingIsWellPlaced", "d8/d3a/locale__format_8hpp.html#aae03a37827071172d1d9397d0323c224", null ],
     [ "morph::render::normalizeLocaleNumber", "d8/d3a/locale__format_8hpp.html#a5447aae2448f6a892fe4a0202c224942", null ],
     [ "morph::render::formatCanonicalNumber", "d8/d3a/locale__format_8hpp.html#a8a5e641c416e175501d315d722c97b0b", null ]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -5,8 +5,9 @@ var searchData=
   ['getwire_2',['getWire',['../d2/d5c/structmorph_1_1math_1_1Rational.html#a7338c559da93d37a286f222ce825931c',1,'morph::math::Rational']]],
   ['greater_3',['greater',['../d4/d11/forms_8hpp.html#a1afefe4ea53ee4ca21c4103f41d7c592',1,'morph::forms']]],
   ['greaterorequal_4',['greaterOrEqual',['../d4/d11/forms_8hpp.html#af3bd1e51e06b51f833c203f904f0d364',1,'morph::forms']]],
-  ['groupkey_5',['groupKey',['../dc/d3c/forms_2i18n_8hpp.html#a8da9e72cd7e319d6900211832537fc60',1,'morph::forms::i18n']]],
-  ['groupkindname_6',['groupKindName',['../dd/d6c/layout_8hpp.html#a828cd5d50c7e0aee899144c6022ed8e8',1,'morph::forms']]],
-  ['guard_7',['guard',['../de/d0d/classmorph_1_1async_1_1CallbackToken.html#a4ac17ad8d956cc7df909f482f2c737da',1,'morph::async::CallbackToken::guard()'],['../d7/d9f/classmorph_1_1async_1_1CallbackScope.html#a5d4df9c68ae850b74193f9c4f060ae90',1,'morph::async::CallbackScope::guard()']]],
-  ['guiexecutor_8',['guiExecutor',['../d4/d7b/classmorph_1_1bridge_1_1BridgeHandler.html#a7cf0765d2e6bdcc796695116019542f4',1,'morph::bridge::BridgeHandler']]]
+  ['groupingiswellplaced_5',['groupingIsWellPlaced',['../d8/d3a/locale__format_8hpp.html#aae03a37827071172d1d9397d0323c224',1,'morph::render::detail']]],
+  ['groupkey_6',['groupKey',['../dc/d3c/forms_2i18n_8hpp.html#a8da9e72cd7e319d6900211832537fc60',1,'morph::forms::i18n']]],
+  ['groupkindname_7',['groupKindName',['../dd/d6c/layout_8hpp.html#a828cd5d50c7e0aee899144c6022ed8e8',1,'morph::forms']]],
+  ['guard_8',['guard',['../de/d0d/classmorph_1_1async_1_1CallbackToken.html#a4ac17ad8d956cc7df909f482f2c737da',1,'morph::async::CallbackToken::guard()'],['../d7/d9f/classmorph_1_1async_1_1CallbackScope.html#a5d4df9c68ae850b74193f9c4f060ae90',1,'morph::async::CallbackScope::guard()']]],
+  ['guiexecutor_9',['guiExecutor',['../d4/d7b/classmorph_1_1bridge_1_1BridgeHandler.html#a7cf0765d2e6bdcc796695116019542f4',1,'morph::bridge::BridgeHandler']]]
 ];
